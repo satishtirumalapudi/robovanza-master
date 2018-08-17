a173d18fc8c36c63eb00a0a29d42b812119b7f0f
miniproject506 <32669922+miniproject506@users.noreply.github.com>
2018-08-17 23:21:07 +0530

    final changes

--- styles/bootstrap4/timer.js
+++ styles/bootstrap4/timer.js
@@ -1,5 +1,5 @@
 function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
+  var t =(new Date("Sept 28, 2018 00:00:00").getTime() - new Date().getTime());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
